a664eba155f36423e80a7d75676da21ea6ac3164
calsionfeng <calsionfeng@tencent.com>
2022-06-21 20:12:28 +0800

    解析 property 编码

--- MachOParser/main.mm
+++ MachOParser/main.mm
@@ -21,6 +21,7 @@
 #define RO_POINTER_OFFSET_STRUCT (4 * DATA_POINTER_OF_BYTE)
 #define PROPERTY_POINTER_OFFSET_STRUCT 64
 #define NAME_POINTER_OFFSET_STRUCT 24
+#define PROPERTY_LIST_ITEM_OF_BYTE 16
 
 // 验证文件是否存在
 bool isFileExists_access(const char *&name) {
@@ -68,7 +69,7 @@ int main(int argc, const char * argv[]) {
             printf("[Error] No __TEXT,__objc_classname inside MachO\n");
             return 1;
         }
-        // 解析 __objc_classname 段
+        // 解析 __objc_classname 段，里面存的是类名字符串，以 \0 划分
         uint64_t classname_addr = classname_section->address();
         printf("__TEXT,__objc_classname section address: 0x%llx\n", classname_addr);
         
@@ -84,6 +85,44 @@ int main(int argc, const char * argv[]) {
         }
     }
 
+    // __TEXT,__objc_methname/__TEXT,__cstring
+    std::unordered_map<uint64_t, std::string> addr2methname;
+    {
+        {
+            LIEF::MachO::Section *objc_methname = arm64_bin->get_section("__objc_methname");
+            if (objc_methname == nullptr || objc_methname->segment_name() != "__TEXT") {
+                printf("[Error] No __TEXT,__objc_methname inside MachO\n");
+                return 1;
+            }
+            uint64_t methname_addr = objc_methname->address();
+            auto methname_content = objc_methname->content();
+            for (auto iter = methname_content.begin(); iter != methname_content.end();) {
+                const char *p = (const char*)iter;
+                std::string tmp_string(p);
+                addr2methname[methname_addr] = tmp_string;
+                auto len = strlen(p);
+                iter += len + 1; // 跳过结束符
+                methname_addr += len + 1;
+            }
+        }
+        {
+            LIEF::MachO::Section *cstring = arm64_bin->get_section("__cstring");
+            if (cstring == nullptr || cstring->segment_name() != "__TEXT") {
+                printf("[Error] No __TEXT,__cstring inside MachO\n");
+                return 1;
+            }
+            uint64_t cstring_addr = cstring->address();
+            auto cstring_content = cstring->content();
+            for (auto iter = cstring_content.begin(); iter != cstring_content.end();) {
+                const char *p = (const char*)iter;
+                std::string tmp_string(p);
+                addr2methname[cstring_addr] = tmp_string;
+                auto len = strlen(p);
+                iter += len + 1; // 跳过结束符
+                cstring_addr += len + 1;
+            }
+        }
+    }
     
     // __DATA section
     LIEF::MachO::Section *objc_data_sec = nullptr;
@@ -104,8 +143,7 @@ int main(int argc, const char * argv[]) {
         for (auto section = it_section.begin(); section != it_section.end(); ++section) {
 //            printf("%s\n", section->fullname().c_str());
             if (section->name() == "__objc_ivar") { // __objc_ivar
-                // span<const uint8_t>
-                // 一个大小为 8 bit 数据
+                // 存储了所有 ivar 的地方，一个大小为 4 字节
                 auto content = section->content();
                 printf("total ivar nums: %lu\n", content.size() / DATA_NUMS_OF_BYTE);
             } else if (section->name() == "__objc_data") { // __objc_data
@@ -125,6 +163,7 @@ int main(int argc, const char * argv[]) {
     std::vector<uint64_t> classrw_addr_list;
     {
         // 解析 __objc_classlist 段，找到类在 __objc_data/__data 的地址
+        // 里面存的是指向每一个类信息的指针，大小为 8 字节
         std::vector<uint64_t> class_addr_list;
         auto objc_classlist_content = objc_classlist_sec->content();
         auto objc_classlist_size = objc_classlist_content.size();
@@ -141,7 +180,7 @@ int main(int argc, const char * argv[]) {
         auto data_content = data_sec->content();
         auto data_base_addr = data_sec->address();
         auto data_end_addr = data_base_addr + data_sec->size();
-        for (auto addr : class_addr_list) {
+        for (auto addr : class_addr_list) { // 解析 __objc_classlist 的指针指向
             if (addr < data_base_addr) { // ro, __DATA,__objc_data
                 auto offset = addr - objc_data_base_addr + RO_POINTER_OFFSET_STRUCT;
                 auto pointer_data = objc_data_content.subspan(offset, DATA_POINTER_OF_BYTE);
@@ -170,6 +209,34 @@ int main(int argc, const char * argv[]) {
         auto objc_data_end_addr = objc_data_base_addr + objc_data_sec->size();
         uint64_t minimun_limit = (1llu << 32); // 前 4GB 为 page zero
         for (auto addr : classro_addr_list) {
+            /*
+             __objc_data(class):
+                 ISA - 8B
+                 Super Class - 8B
+                 Cache - 8B
+                 Vtable - 8B
+                 Data - 8B - 指向 __objc_const/__objc_data 的指针
+
+             __objc_const/__objc_data(class info):
+                Flags - 4B
+                Instance Start - 4B
+                Instance Size - 4B
+                Reserved - 4B
+                Instance Var Layout - 8B - 指向 __objc_classname 的数据，
+                                    代表 OC 的 GC 对于 strong 和 weak 的 ivars 的一个 bitmap 的 layout(布局)描述
+                Name - 8B - 指向 __objc_classname 类名的指针
+                Base Methods - 8B - 指向 method list 的指针
+                Base Protocols - 8B
+                Instance Variables - 8B - 指向 Variable List 的指针，数据里面 Offset 指向 __objc_ivar
+                Weak Instance Var Layout - 8B
+                Base Properties - 8B - 指向 property list 的指针
+
+             property list:
+                Entry Size - 4B
+                Count - 4B
+                Name - 8B
+                Attributes - 8B
+             */
             uint64_t property_ptr = 0;
             uint64_t classname_ptr = 0;
             if (addr < objc_data_base_addr) { // __objc_const
@@ -194,17 +261,32 @@ int main(int argc, const char * argv[]) {
             } else {
                 abort();
             }
-            assert(classname_ptr == 0 || addr2classname.find(classname_ptr) != addr2classname.end());
+            assert(classname_ptr != 0);
+            assert(addr2classname.find(classname_ptr) != addr2classname.end());
             
             if (property_ptr == 0) {
-                printf("class: %s, prop nums: 0\n", addr2classname[classname_ptr].c_str());
+                printf("\nclass: %s, prop nums: 0\n", addr2classname[classname_ptr].c_str());
                 continue;
             }
             
             auto prop_list_offset = property_ptr - objc_const_base_addr + DATA_NUMS_OF_BYTE;
             auto property_count = convert_little_endian(objc_const_content.subspan(prop_list_offset, DATA_NUMS_OF_BYTE), DATA_NUMS_OF_BYTE);
-            printf("class: %s, prop nums: %llu\n", addr2classname[classname_ptr].c_str(), property_count);
+            printf("\nclass: %s, prop nums: %llu\n", addr2classname[classname_ptr].c_str(), property_count);
             total_prop_nums += property_count;
+
+            // 解析出所有的 property
+            for (int i = 0; i < property_count; ++i) {
+                auto cur_offset = prop_list_offset + DATA_NUMS_OF_BYTE + (PROPERTY_LIST_ITEM_OF_BYTE * i);
+                auto property_name_addr = convert_little_endian(objc_const_content.subspan(cur_offset, DATA_POINTER_OF_BYTE), DATA_POINTER_OF_BYTE);
+                auto property_attribute_addr = convert_little_endian(objc_const_content.subspan(cur_offset + DATA_POINTER_OF_BYTE, DATA_POINTER_OF_BYTE), DATA_POINTER_OF_BYTE);
+
+                assert(property_name_addr != 0);
+                assert(addr2methname.find(property_name_addr) != addr2methname.end());
+                assert(property_attribute_addr != 0);
+                assert(addr2methname.find(property_attribute_addr) != addr2methname.end());
+
+                printf("Name: %s, Attribute: %s\n", addr2methname[property_name_addr].c_str(), addr2methname[property_attribute_addr].c_str());
+            }
         }
         // 没啥用，rw 全是运行时补全的信息
 //        // rw，复制一遍
@@ -236,6 +318,6 @@ int main(int argc, const char * argv[]) {
 //            total_prop_nums += property_count;
 //        }
     }
-    printf("total custom class prop nums: %llu\n", total_prop_nums);
+    printf("\ntotal custom class prop nums: %llu\n", total_prop_nums);
     return 0;
 }
